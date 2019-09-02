1f59a5c1abf760fa2910ce0e5b310bccbe3525c9
eyenoon <eyenoon20@gmail.com>
2019-09-03 01:18:14 +0900

    1. 식당추가 api 수정

--- src/main/java/pe/oh29oh29/ourlunch/domain/family/FamilyRepository.java
+++ src/main/java/pe/oh29oh29/ourlunch/domain/family/FamilyRepository.java
@@ -3,4 +3,5 @@ package pe.oh29oh29.ourlunch.domain.family;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FamilyRepository extends JpaRepository<Family, Long> {
+    Family findByName(String familyName);
 }
--- src/main/java/pe/oh29oh29/ourlunch/domain/restaurant/RestaurantService.java
+++ src/main/java/pe/oh29oh29/ourlunch/domain/restaurant/RestaurantService.java
@@ -2,15 +2,28 @@ package pe.oh29oh29.ourlunch.domain.restaurant;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pe.oh29oh29.ourlunch.domain.family.Family;
+import pe.oh29oh29.ourlunch.domain.family.FamilyRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final FamilyRepository familyRepository;
 
-    void addRestaurant() {
+    void addRestaurant(final String familyName,
+                       final String restaurantName,
+                       final String positionX,
+                       final String positionY) {
 
+        Family family = familyRepository.findByName(familyName);
+        Restaurant restaurant = restaurantRepository.save(Restaurant.newInstance(family, restaurantName, positionX, positionY));
+        List<Restaurant> restaurantList = family.getRestaurant();
+        restaurantList.add(restaurant);
+        family.setRestaurant(restaurantList);
     }
 
 }

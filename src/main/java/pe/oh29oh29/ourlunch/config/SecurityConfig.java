c8575879e0b48ed21713acad85790f0128ce46f2
oh29oh29 <hyukjae89@gmail.com>
2019-09-04 00:21:28 +0900

    OAuth2 인증을 위한 초안

---
+++ src/main/java/pe/oh29oh29/ourlunch/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package pe.oh29oh29.ourlunch.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll();
+
+
+    }
+}

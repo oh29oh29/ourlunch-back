eb06057101c800c46673db12b10d687d914cb6bb
goodListener <good_listener@naver.com>
2020-03-28 01:27:56 +0900

    Security dev에 cors 세팅 추가

--- src/main/java/pe/oh29oh29/ourlunch/config/SecurityDevConfig.java
+++ src/main/java/pe/oh29oh29/ourlunch/config/SecurityDevConfig.java
@@ -1,11 +1,16 @@
 package pe.oh29oh29.ourlunch.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Profile("dev")
 @Configuration
@@ -30,10 +35,25 @@ public class SecurityDevConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll().and().cors();
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
